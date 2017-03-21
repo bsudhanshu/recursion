e2258a51af4f268efd03b476c4baa848f71923bc
Bariz <bsudhanshu@gmail.com>
2017-03-21 00:40:27 -0700

    Add function for stringifyJSON and make all tests pass

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,30 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === "number" || typeof obj === "boolean") {
+    return obj.toString();
+  } else if (obj === null) {
+	return "null";
+  } else if (typeof obj === "string") {
+    return '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+  	  var result = [];
+  	  if (obj.length === 0) {
+  	  return "[]";
+  	  }
+  	  obj.forEach(function(ele){
+        result.push(stringifyJSON(ele));
+  	  });
+    return '[' + result + ']';
+  } else if (typeof obj === "object") {
+      var result = [];
+      for (var key in obj) {
+      	if (typeof obj[key] === "undefined" || typeof obj[key] === "function") {
+        return "{}";
+        }
+        result.push(stringifyJSON(key) + ":" + stringifyJSON(obj[key]));
+      }
+    return "{" + result + "}";
+  }
+
 };
